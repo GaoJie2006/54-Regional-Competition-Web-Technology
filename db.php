644b3a8c4eff85f0bccc6a953a96a6318e71af84
Gaojie2006 <106969990+Gaojie2006@users.noreply.github.com>
2024-01-19 14:18:13 +0800

    新增 資料庫操作、輔助 函數庫

---
+++ db.php
@@ -0,0 +1,91 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+$db = mysqli_connect("127.0.0.1", "admin", "1234", "web54");
+
+if (!$db) {
+    die(mysqli_connect_error());
+}
+
+$db->query("SET NAMES UTF8");
+
+function ex($data, $s = ",", $c = "=")
+{
+    global $db;
+    $arrar = [];
+    if (is_array($data)) {
+        foreach ($data as $k => $v) {
+            $v = htmlspecialchars($db->real_escape_string("$v"));
+            $array[] = "`$k` $c '$v'";
+        }
+    } else {
+        $array[] = $data;
+    }
+    $array = join($s, $array);
+    return $array;
+}
+
+function sel($table, $where = 1)
+{
+    global $db;
+    $w = ex($where, "AND");
+    $sql = "SELECT * FROM `$table` WHERE $w";
+    $result = $db->query($sql);
+    return $result->fetch_assoc();
+}
+
+function sels($table, $where = 1)
+{
+    global $db;
+    $w = ex($where);
+    $sql = "SELECT * FROM `$table` WHERE $w";
+    $result = $db->query($sql);
+    $array = [];
+    while ($r = $result->fetch_assoc()) {
+        $array[] = $r;
+    }
+    return $array;
+}
+
+function ins($table,$data)
+{
+    global $db;
+    $d = ex($data);
+    $sql = "INSERT INTO `$table` SET $d";
+    $db->query($sql);
+    return $db->insert_id;
+}
+
+function upd($table, $data, $where)
+{
+    global $db;
+    $d = ex($data);
+    $w = ex($where, "AND");
+    $sql = "UPDATE `$table` SET $d WHERE $w";
+    $db->query($sql);
+}
+
+function del($table, $where)
+{
+    global $db;
+    $w = ex($where, "AND");
+    $sql = "DELETE FROM `$table` WHERE $w";
+    $db->query($sql);
+}
+
+function dd($data)
+{
+    echo "<pre>";
+    var_dump($data);
+    echo "</pre>";
+    die();
+}
+function redirect($href){
+    header("location: $href");
+}
+
+function alert($mess, $href)
+{
+    echo "<script>alert('$mess');location.href='$href'</script>";
+}
