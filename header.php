62f5248977afe80f1cd1b557187a68aa125ee394
Gaojie2006 <106969990+Gaojie2006@users.noreply.github.com>
2024-01-19 14:20:38 +0800

    新增 標頭設定文件 與 Navbar

---
+++ header.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>快樂旅遊網</title>
+<link rel="stylesheet" href="./css/bootstrap.css">
+<link rel="stylesheet" href="./css/style.css">
+<script src="./js/jquery.min.js"></script>
+<script src="./js/bootstrap.js"></script>
+<script src="./js/app.js"></script>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once('header.php') ?>
+</head>
+<body>
+    <?php require_once('nav.php') ?>
+</body>
+</html>
