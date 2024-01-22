8c41ae5b56e254d68a2d635a0b6f82addf5a12c5
Gaojie2006 <106969990+Gaojie2006@users.noreply.github.com>
2024-01-22 15:56:22 +0800

    更改圖片驗證寫法

--- captcha.php
+++ captcha.php
@@ -1,19 +1,9 @@
 <?php
-session_start();
-$width = 60;
-$height = 40;
-
-$image = imagecreatetruecolor($width, $height);
-$white = imagecolorallocate($image, 255, 255, 255);
-$black = imagecolorallocate($image, 0, 0, 0);
-
-$text = str_pad(rand(0,9999),4,"0",STR_PAD_LEFT);
-$_SESSION['captcha'] = $text;
-
-$font = "ARIAL.TTF";
-imagefilledrectangle($image,0,0,$white-1,$height-1,$white);
-imagefttext($image,20,0,0,30,$black,$font,$text);
-
-header('Content-Type: image/png');
-imagepng($image);
-imagedestroy($image);
+    session_start();
+    header('Content-type:image/svg+xml');
+    $text = str_pad(rand(0,9999),4,"0",STR_PAD_LEFT);
+    $_SESSION['captcha'] = $text;
+?>
+<svg xmlns="http://www.w3.org/2000/svg" width="120" height="30">
+    <text x="0" y="23" style="font-size:30px;"><?=$text?></text>
+</svg>
